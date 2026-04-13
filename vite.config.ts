367636946e883a9d79bd0d9212e7b9ba3f13b6fe
TeAdmin <Techadmin@teckexplorers.com>
2026-04-13 15:12:50 +0530

    Updated Vite Config changed base path...

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/TE-Weather-App-V2/',
+  base: '/',
   plugins: [react()],
   build: {
     cssMinify: false,
